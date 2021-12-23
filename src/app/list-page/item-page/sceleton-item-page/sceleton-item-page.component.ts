5f5e93d5ce4e65aad780bd93b9a706c2b93dcec0
Sannet0 <shupletsov.exceedteam@gmail.com>
2021-12-23 16:44:11 +0300

    fix styles

--- src/app/list-page/item-page/sceleton-item-page/sceleton-item-page.component.ts
+++ src/app/list-page/item-page/sceleton-item-page/sceleton-item-page.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sceleton-item-page',
   templateUrl: './sceleton-item-page.component.html',
-  styleUrls: ['./sceleton-item-page.component.scss']
+  styleUrls: ['./sceleton-item-page.component.scss', '../item-page.component.scss']
 })
 export class SceletonItemPageComponent {}
